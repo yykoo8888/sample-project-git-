49a627f94565cfc7768c13a4b9368d0087ede48d
Yun Ye <yunye.koo10@rafflesgirlssch.edu.sg>
2023-05-31 11:31:36 +0800

    changed "world" to "banana", changed image code to butterfly emoji text :)

--- git thing/ContentView.swift
+++ git thing/ContentView.swift
@@ -10,10 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("🦋")
+            Text("Hello, banana!")
         }
         .padding()
     }
