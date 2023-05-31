02d3bd42e1040896237f0c0ed048421d425c5664
Yun Ye <yunye.koo10@rafflesgirlssch.edu.sg>
2023-05-31 11:08:23 +0800

    Initial Commit

---
+++ git thing/git_thingApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_thingApp.swift
+//  git thing
+//
+//  Created by Koo  on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct git_thingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
